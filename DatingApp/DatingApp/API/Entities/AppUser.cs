99609c2f29f695ab05b423ec9311363bfdd4be01
Filip <filipmcsg@gmail.com>
2022-04-12 21:22:48 +0200

    created api folder and moved everything inside to look more organized, added ef core sqlite nuget, added data context class to derive from dbcontext

--- DatingApp/DatingApp/API/Entities/AppUser.cs
+++ DatingApp/DatingApp/API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,8 +10,6 @@ namespace DatingApp.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-
-
 
     }
 }
