fe886b745b86ddacaacd33b663a8baaaea2d81fd
Filip <filipmcsg@gmail.com>
2022-05-05 10:08:39 +0200

    added logic to token service

--- DatingApp/DatingApp/API/Services/TokenService.cs
+++ DatingApp/DatingApp/API/Services/TokenService.cs
@@ -4,7 +4,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,7 +21,25 @@ namespace DatingApp.API.Services
         }
         public string CreateToken(AppUser user)
         {
-            //wip
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
