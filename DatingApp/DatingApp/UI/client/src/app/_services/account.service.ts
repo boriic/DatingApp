36e440ec95f9ef48b77e027ce826af5311c5a6fd
Filip <filipmcsg@gmail.com>
2022-05-09 22:19:03 +0200

    refactored frontend files, added feature to stay logged in after refreshing the page

--- DatingApp/DatingApp/UI/client/src/app/_services/account.service.ts
+++ DatingApp/DatingApp/UI/client/src/app/_services/account.service.ts
@@ -1,17 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, ReplaySubject } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
-baseUrl = 'https://localhost:44320/api/'
+  baseUrl = 'https://localhost:44320/api/';
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
 
-   }
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
 
-   login(model:any){
-     return this.http.post(this.baseUrl + 'account/login', model);
-   }
+  setCurrentUser(user: User){
+    this.currentUserSource.next(user);
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
 }
